2571c3feeece2cbea1a3b92ece22cdaa8d2a8893
Saqib Ahmad <you@example.com>
2022-12-24 09:03:19 +0600

    counter app made using redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { decrement, increaseByAmount, increment, reset } from './features/counterSlice';
 
 function App() {
+  const count = useSelector(state => state.counter.count)
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App</h1>
+      <h3>Count: {count}</h3>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(reset())}>reset</button>
+      <button onClick={() => dispatch(increaseByAmount(10))}>increaseBy10</button>
     </div>
   );
 }
